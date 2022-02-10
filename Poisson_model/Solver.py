e6533ec07061b3c748779b34636d3862af13740d
Daniel-Huang <dzhuang@caltech.edu>
2022-02-10 09:50:13 -0800

    add exact Jacobian, works

--- Poisson_model/Solver.py
+++ Poisson_model/Solver.py
@@ -83,7 +83,12 @@ def implicit_solve(model_jac, f, dbc, dt = 1.0, Nt = 1000, save_every = 1, L = 1
     for i in range(1, Nt+1): 
         
         # this include dt in both V
-        model_jac(q, yy, dt, res, V)
+        model_jac(q, yy, res, V)
+        # print("error : ", np.linalg.norm(sparse.coo_matrix((V,(I,J)),shape=(Ny-2,Ny-2)).tocsc() * q[1:Ny-1] - res) )
+        
+        V *= -dt
+        V[0:-1:3] += 1.0
+        
         A = sparse.coo_matrix((V,(I,J)),shape=(Ny-2,Ny-2)).tocsc()
         q[1:Ny-1] += spsolve(A, dt*(f[1:Ny-1] + res))
 
@@ -94,6 +99,83 @@ def implicit_solve(model_jac, f, dbc, dt = 1.0, Nt = 1000, save_every = 1, L = 1
             print(i, "max q: ", np.max(q), " L2 res: ", np.linalg.norm(f[1:Ny-1] + res))
 
     return  yy, t_data, q_data
+
+
+# the model is a function: q,t ->  M(q)
+# the solution points are at cell faces
+def implicit_Newton_solve(model_jac, f, dbc, dt = 1.0, Nt = 1000, save_every = 1, L = 1.0, Newton_eps = 1e-3, Newton_maxiterstep = 1000):
+    
+    
+    
+    Ny = f.size
+    yy = np.linspace(0, L, Ny)
+    
+    t = 0.0
+    # qi are periodic (qi[0] == qi[-1])
+    q = np.linspace(dbc[0], dbc[1], Ny)
+    # q = -yy*(yy - 1)
+    # q[0], q[-1] = dbc[0], dbc[1]
+    
+    # prepare data storage
+    q_data = np.zeros((Nt//save_every+1, Ny))
+    t_data = np.zeros(Nt//save_every+1)
+    q_data[0, :], t_data[0] = q, t
+
+    I, J = np.zeros(3*(Ny-2)-2), np.zeros(3*(Ny-2)-2)
+    for i in range(Ny-2):
+        if i == 0:
+            I[0], I[1] = i, i 
+            J[0], J[1] = i, i+1
+        elif i == Ny-3:
+            I[2 + 3*(i - 1) + 0], I[2 + 3*(i - 1) + 1] = i, i
+            J[2 + 3*(i - 1) + 0], J[2 + 3*(i - 1) + 1] = i-1, i
+        else:
+            I[2 + 3*(i - 1) + 0], I[2 + 3*(i - 1) + 1], I[2 + 3*(i - 1) + 2] = i, i, i
+            J[2 + 3*(i - 1) + 0], J[2 + 3*(i - 1) + 1], J[2 + 3*(i - 1) + 2] = i-1, i, i+1
+            
+            
+    res = np.zeros(Ny-2)  
+    V = np.zeros(3*(Ny-2)-2)
+        
+    for i in range(1, Nt+1): 
+        
+        # this include dt in both V
+        
+        Newton_iterstep, Newton_converge = 0, False
+        
+        q_old = np.copy(q)
+        
+        while  not Newton_converge:
+
+            Newton_iterstep += 1
+            
+            model_jac(q, yy, res, V)
+            
+            V *= -dt
+            V[0:-1:3] += 1.0
+            A = sparse.coo_matrix((V,(I,J)),shape=(Ny-2,Ny-2)).tocsc()
+
+            res_all = q[1:Ny-1] - q_old[1:Ny-1]  - dt*(f[1:Ny-1] + res)
+            
+            delta_q = spsolve(A, res_all)
+
+            q[1:Ny-1] -= delta_q
+            # @show Newton_iterstep, norm(res)
+            if (np.linalg.norm(res_all) < Newton_eps  or Newton_iterstep > Newton_maxiterstep):
+                if Newton_iterstep > Newton_maxiterstep:
+                    print("Newton iteration does not converge :", Newton_iterstep, " error is : ", np.linalg.norm(res_all))
+                Newton_converge = True
+            
+
+
+        
+        if i%save_every == 0:
+            q_data[i//save_every, :] = q
+            t_data[i//save_every] = i*dt
+            print(i, "max q: ", np.max(q), " L2 res: ", np.linalg.norm(res_all))
+
+    return  yy, t_data, q_data
+
 
 
  
@@ -107,26 +189,45 @@ def nummodel(permeability, q, yy, res):
     res[:] = gradient_first_c2f(mu_c*(dq_c), dy)
     
 
-def nummodel_jac(permeability, q, yy, dt,  res, V):
+def nummodel_jac(permeability, q, yy, res, V, exact = False, D_permeability = None):
     
     Ny = yy.size
     dy = yy[1] - yy[0]
     dq_c = gradient_first_f2c(q, dy)
     q_c = interpolate_f2c(q) 
     mu_c = permeability(q_c, dq_c)
+    # i -> i-1/2
+    #   q: 0 ---- 1 ---- 2 ---- 3 ---- 4   ...   ---- Ny-2 ---- Ny-1
+    # q_c:     0      1      2      3      ...             Ny-2
+    # jac:        0      1      2      3   ...        Ny-3   
+    
     
     res[:] = gradient_first_c2f(mu_c*(dq_c), dy)
     
     V[:] = 0
     
+   
     for i in range(Ny-2):
         if i == 0:
-            V[0], V[1] =  1 + dt/dy**2 * (mu_c[i] +  mu_c[i+1]), -dt/dy**2 * mu_c[i+1]
+            V[0], V[1] =  -1/dy**2 * (mu_c[i] +  mu_c[i+1]), 1/dy**2 * mu_c[i+1]
         elif i == Ny-3:
-            V[2 + 3*(i - 1) + 0], V[2 + 3*(i - 1) + 1] = -dt/dy**2 * mu_c[i], 1 + dt/dy**2 * (mu_c[i] +  mu_c[i+1])
+            V[2 + 3*(i - 1) + 0], V[2 + 3*(i - 1) + 1] = 1/dy**2 * mu_c[i], -1/dy**2 * (mu_c[i] +  mu_c[i+1])
         else:
-            V[2 + 3*(i - 1) + 0], V[2 + 3*(i - 1) + 1], V[2 + 3*(i - 1) + 2] = -dt/dy**2 * mu_c[i], 1 + dt/dy**2 * (mu_c[i] +  mu_c[i+1]), -dt/dy**2 * mu_c[i+1]
-      
+            V[2 + 3*(i - 1) + 0], V[2 + 3*(i - 1) + 1], V[2 + 3*(i - 1) + 2] = 1/dy**2 * mu_c[i], -1/dy**2 * (mu_c[i] +  mu_c[i+1]), 1/dy**2 * mu_c[i+1]
+    if exact:
+        Dq_c, Ddq_c = D_permeability(q_c, dq_c)
+        
+        for i in range(Ny-2):
+            # diagonal i,i : q_{i+1}
+            V[3*i] += (q[i+2] - q[i+1])/dy**2 * (1/2 * Dq_c[i+1] - Ddq_c[i+1]/dy) - (q[i+1] - q[i])/dy**2 * (1/2 * Dq_c[i] + Ddq_c[i]/dy)
+        for i in range(Ny-3):
+            # +1 diagonal
+            V[3*i + 1] += (q[i+2] - q[i+1])/dy**2 * (1/2 * Dq_c[i+1] + Ddq_c[i+1]/dy)
+            # -1 diagonal
+            V[3*(i + 1) - 1] -= (q[i+2] - q[i+1])/dy**2 * (1/2 * Dq_c[i+1] - Ddq_c[i+1]/dy)
+        
+            
+          
     return res, V
 
 
--- Poisson_model/SolverTest.py
+++ Poisson_model/SolverTest.py
@@ -4,34 +4,40 @@ import numpy as np
 from scipy.sparse.linalg import spsolve
 from scipy import sparse
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from Solver import nummodel, nummodel_jac, explicit_solve, implicit_solve
+from Solver import nummodel, nummodel_jac, explicit_solve, implicit_solve, implicit_Newton_solve
 
 
 Ny = 200
 L = 1.0
 yy, dy = np.linspace(0, L, Ny), L/(Ny - 1)
 
-TEST = "D=theta**2+dtheta**2+1-case2"
+TEST = "D=dtheta**2-case2" #"D=theta**2+dtheta**2+1-case2"
 if TEST == "Linear":
     def permeability(q, dq):
-        return 0.5  + np.zeros(dq.size)   
+        return 0.5  + np.zeros(dq.size)  
+    def D_permeability(q, dq):
+        return np.zeros(dq.shape), np.zeros(dq.shape)
     f = np.sin(2*np.pi*yy)
     q_sol = 1/0.5 * 1/(2*np.pi)**2 * f
     dbc = np.array([0.0, 0.0])
     
 elif TEST == "D=dtheta**2-case1":
-    # require very small time step: explicit dt = 5.0e-6, Nt = 1000000
+    
     def permeability(q, dq):
         return dq**2
+    def D_permeability(q, dq):
+        return np.zeros(dq.shape), 2*dq
     f = 6*(-2*yy + 1)**2
     q_sol = -yy*(yy - 1)
     dbc = np.array([0.0, 0.0])
     
 elif TEST == "D=dtheta**2-case2":
-    # require very small time step: explicit dt = 1.0e-7, Nt = 200000
+    
     def permeability(q, dq):
         return dq**2  
+    def D_permeability(q, dq):
+        return np.zeros(dq.shape), 2*dq
+    
     f = 3 * np.pi**4 * np.sin(np.pi * yy)**2 * np.cos(np.pi * yy)
     q_sol = np.cos(np.pi * yy)
     dbc = np.array([1.0, -1.0])
@@ -40,33 +46,39 @@ elif TEST == "D=dtheta**2+1-case1":
     # works: implicit dt = 1.0e-3, Nt = 1000
     def permeability(q, dq):
         return dq**2 + 1
+    def D_permeability(q, dq):
+        return np.zeros(dq.shape), 2*dq
     f = 6*(-2*yy + 1)**2 + 2.0
     q_sol = -yy*(yy - 1)
     dbc = np.array([0.0, 0.0])
     
 elif TEST == "D=dtheta**2+1-case2":
-    # require very small time step: explicit dt = 1.0e-7, Nt = 100000
-    # require very small time step: implicit dt = 1.0e-6, Nt = 100000
+    
     def permeability(q, dq):
         return dq**2 + 1
+    def D_permeability(q, dq):
+        return np.zeros(dq.shape), 2*dq
     f = 3 * np.pi**4 * np.sin(np.pi * yy)**2 * np.cos(np.pi * yy) + np.cos(np.pi*yy) * np.pi**2
     q_sol = np.cos(np.pi * yy)
     dbc = np.array([1.0, -1.0]) 
         
 elif TEST == "D=theta**2+dtheta**2+1-case1":
-    # works: implicit dt = 1.0e-3, Nt = 1000
+
     def permeability(q, dq):
         return q**2 + dq**2 + 1
+    def D_permeability(q, dq):
+        return 2*q, 2*dq
     f = 6*(1-2*yy)**2 - 2*(yy - yy**2)*(1 - 2*yy)**2 + 2*(yy - yy**2)**2 + 2 
      
     q_sol = -yy*(yy - 1)
     dbc = np.array([0.0, 0.0]) 
   
 elif TEST == "D=theta**2+dtheta**2+1-case2":
-    # require very small time step: explicit dt = 1.0e-7, Nt = 100000
-    # require very small time step: implicit dt = 1.0e-6, Nt = 20000
+
     def permeability(q, dq):
         return q**2 + dq**2 + 1
+    def D_permeability(q, dq):
+        return 2*q, 2*dq
     c , s = np.cos(np.pi*yy), np.sin(np.pi*yy)
     f = np.pi**2*c**3 - 2*np.pi**2*s**2*c + 3*np.pi**4*s**2*c + np.pi**2*c
     
@@ -78,7 +90,7 @@ elif TEST == "D=theta**2+dtheta**2+1-case2":
 
 
 yy = np.linspace(0, L, Ny)
-MODEL = "imp_nummodel"
+MODEL = "imp_Newton_nummodel"
 
 
 if MODEL == "exp_nummodel":
@@ -88,9 +100,14 @@ if MODEL == "exp_nummodel":
 
 elif MODEL == "imp_nummodel":
     
-    model = lambda q, yy, dt, res, V : nummodel_jac(permeability, q, yy, dt, res, V)  
-    yy, t_data, q_data = implicit_solve(model, f, dbc, dt = 1.0e-6, Nt = 20000, save_every = 1, L = L)
-       
+    model = lambda q, yy, res, V : nummodel_jac(permeability, q, yy, res, V, True, D_permeability)  
+    yy, t_data, q_data = implicit_solve(model, f, dbc, dt = 1.0e-3, Nt = 200, save_every = 1, L = L)
+
+elif MODEL == "imp_Newton_nummodel":
+    
+    model = lambda q, yy, res, V : nummodel_jac(permeability, q, yy, res, V, True, D_permeability)  
+    yy, t_data, q_data = implicit_Newton_solve(model, f, dbc, dt = 1.0e-3, Nt = 200, save_every = 1, L = L, Newton_eps = 1e-3, Newton_maxiterstep = 1000)
+           
 else:
     print("ERROR")
 
